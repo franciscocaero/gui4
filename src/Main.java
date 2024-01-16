4ffae30397db7f3e63e00f51017b1d32ebf074c3
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-16 09:42:39 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PanelDibujo panel=new PanelDibujo();
+        JFrame aplicacion=new JFrame();
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        aplicacion.add(panel);
+        aplicacion.setSize(250,250);
+        aplicacion.setVisible(true);
     }
 }
